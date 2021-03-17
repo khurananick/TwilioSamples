cefe89f17f94a2537440c57ed9350735a649a5d1
khurananick <khurananick@gmail.com>
2021-03-17 17:25:19 -0400

    Update PKCV.java

--- PKCV.java
+++ PKCV.java
@@ -25,72 +25,71 @@ public class Example {
     public static String signingKeySecret;
 
     public static void main(String[] args) throws Exception {
-		// Read Private Key.
-		File filePrivateKey = new File("private.key");
-    if(filePrivateKey.exists()) {
-      FileInputStream fis = new FileInputStream("private.key");
-      byte[] encodedPrivateKey = new byte[(int) filePrivateKey.length()];
-      fis.read(encodedPrivateKey);
-      fis.close();
-      KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-      PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(encodedPrivateKey);
+        // Read Private Key.
+        File filePrivateKey = new File("private.key");
+        if (filePrivateKey.exists()) {
+            FileInputStream fis = new FileInputStream("private.key");
+            byte[] encodedPrivateKey = new byte[(int) filePrivateKey.length()];
+            fis.read(encodedPrivateKey);
+            fis.close();
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(encodedPrivateKey);
 
-		  privateKey = keyFactory.generatePrivate(privateKeySpec);
-      keySid = new String(Files.readAllBytes(Paths.get("keySid.txt")));
-      signingKeySid = new String(Files.readAllBytes(Paths.get("signingKeySid.txt")));
-      signingKeySecret = new String(Files.readAllBytes(Paths.get("signingKeySecret.txt")));
-    }
-    else {
-        // Generate public/private key pair
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(2048);
-        KeyPair pair = keyGen.generateKeyPair();
-        java.security.PublicKey pk = pair.getPublic();
+            privateKey = keyFactory.generatePrivate(privateKeySpec);
+            keySid = new String(Files.readAllBytes(Paths.get("keySid.txt")));
+            signingKeySid = new String(Files.readAllBytes(Paths.get("signingKeySid.txt")));
+            signingKeySecret = new String(Files.readAllBytes(Paths.get("signingKeySecret.txt")));
+        } else {
+            // Generate public/private key pair
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(2048);
+            KeyPair pair = keyGen.generateKeyPair();
+            java.security.PublicKey pk = pair.getPublic();
 
-        // Use the default rest client
-        TwilioRestClient client =
-            new TwilioRestClient.Builder(ACCOUNT_SID, AUTH_TOKEN)
+            // Use the default rest client
+            TwilioRestClient client =
+                new TwilioRestClient.Builder(ACCOUNT_SID, AUTH_TOKEN)
                 .build();
 
-        // Create a public key and signing key using the default client
-        PublicKey key = PublicKey.creator(
-            Base64.encodeBase64String(pk.getEncoded())
-        ).setFriendlyName("Public Key").create(client);
+            // Create a public key and signing key using the default client
+            PublicKey key = PublicKey.creator(
+                Base64.encodeBase64String(pk.getEncoded())
+            ).setFriendlyName("Public Key").create(client);
 
-        NewSigningKey signingKey = NewSigningKey.creator().create(client);
+            NewSigningKey signingKey = NewSigningKey.creator().create(client);
 
-        X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(pair.getPublic().getEncoded());
-        FileOutputStream fos = new FileOutputStream("public.key");
-        fos.write(x509EncodedKeySpec.getEncoded());
-        fos.close();
+            X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(pair.getPublic().getEncoded());
+            FileOutputStream fos = new FileOutputStream("public.key");
+            fos.write(x509EncodedKeySpec.getEncoded());
+            fos.close();
 
-        privateKey = pair.getPrivate();
-        PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(privateKey.getEncoded());
-        fos = new FileOutputStream("private.key");
-        fos.write(pkcs8EncodedKeySpec.getEncoded());
-        fos.close();
+            privateKey = pair.getPrivate();
+            PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(privateKey.getEncoded());
+            fos = new FileOutputStream("private.key");
+            fos.write(pkcs8EncodedKeySpec.getEncoded());
+            fos.close();
 
-        keySid = key.getSid();
-        fos = new FileOutputStream("keySid.txt");
-        fos.write(keySid.getBytes());
-        fos.close();
+            keySid = key.getSid();
+            fos = new FileOutputStream("keySid.txt");
+            fos.write(keySid.getBytes());
+            fos.close();
 
-        signingKeySid = signingKey.getSid();
-        fos = new FileOutputStream("signingKeySid.txt");
-        fos.write(signingKeySid.getBytes());
-        fos.close();
+            signingKeySid = signingKey.getSid();
+            fos = new FileOutputStream("signingKeySid.txt");
+            fos.write(signingKeySid.getBytes());
+            fos.close();
 
-        signingKeySecret = signingKey.getSecret();
-        fos = new FileOutputStream("signingKeySecret.txt");
-        fos.write(signingKeySecret.getBytes());
-        fos.close();
-    }
+            signingKeySecret = signingKey.getSecret();
+            fos = new FileOutputStream("signingKeySecret.txt");
+            fos.write(signingKeySecret.getBytes());
+            fos.close();
+        }
 
-      // Switch to validation client as the default client
-      TwilioRestClient validationClient = new TwilioRestClient.Builder(signingKeySid, signingKeySecret)
-          .accountSid(ACCOUNT_SID)
-          .httpClient(new ValidationClient(ACCOUNT_SID, keySid, signingKeySid, privateKey))
-          .build();
+        // Switch to validation client as the default client
+        TwilioRestClient validationClient = new TwilioRestClient.Builder(signingKeySid, signingKeySecret)
+            .accountSid(ACCOUNT_SID)
+            .httpClient(new ValidationClient(ACCOUNT_SID, keySid, signingKeySid, privateKey))
+            .build();
 
         Message message = Message.creator(
             new PhoneNumber("+1908"), // to
