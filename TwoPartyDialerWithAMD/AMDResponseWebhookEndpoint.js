20ae3ff62c5e765dda8e9d4bbf1308f53c7defb0
khurananick <khurananick@gmail.com>
2021-04-27 20:13:36 -0400

    Update AMDResponseWebhookEndpoint.js

--- TwoPartyDialerWithAMD/AMDResponseWebhookEndpoint.js
+++ TwoPartyDialerWithAMD/AMDResponseWebhookEndpoint.js
@@ -1,9 +1,15 @@
+// the webhook endpoint called by answering machine detection.
+
 const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-client.calls(req.query.agentCallSid)
+if(req.body.AnsweredBy == 'machine_start') {
+  // lets the agent know there was voicemail detected.
+  client.calls(req.query.agentCallSid)
       .update({twiml: '<Response><Say>Voicemail Detected. Goodbye.</Say></Response>'})
       .then(call => console.log(call.to));
 
-client.calls(req.body.CallSid)
+  // leaves voicemail for the customer.
+  client.calls(req.body.CallSid)
       .update({twiml: '<Response><Say loop="2">This is a voicemail from our team. Please follow these instructions.</Say></Response>'})
       .then(call => console.log(call.to));
+}
