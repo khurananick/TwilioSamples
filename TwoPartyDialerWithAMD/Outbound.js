379680b8a0ea848c266d50c249a833f98ba847aa
khurananick <khurananick@gmail.com>
2021-04-27 20:12:02 -0400

    Update Outbound.js

--- TwoPartyDialerWithAMD/Outbound.js
+++ TwoPartyDialerWithAMD/Outbound.js
@@ -1,9 +1,10 @@
 const client = require('twilio')('ACXXX', '55XXX');
-const agentNum = "+14083334444";
-const userNum = "+14084445555";
-const callerId = "+14082223333";
+const agentNum = "+14083334444"; // number of the agent.
+const userNum = "+14084445555"; // number of the customer.
+const callerId = "+14082223333"; // the Twilio number to show in Caller ID.
 
 (async function() {
+  // creates the outbound call leg to the Agent and puts the Agent into a queue.
   const agentCall = await client.calls
     .create({
       twiml: `<Response><Enqueue>${userNum}</Enqueue></Response>`,
@@ -12,11 +13,12 @@ const callerId = "+14082223333";
      });
 
   setTimeout(async function() {
+    // creates the outbound call leg to the Customer, then dials the queue in which the Agent is waiting.
     const userCall = await client.calls
       .create({
         twiml: `<Response><Dial><Queue>${userNum}</Queue></Dial></Response>`,
          machineDetection: 'Enable',
-         asyncAmdStatusCallback: `https://yourdomain.com/path/to/amd/response?agentCallSid=${agentCall.sid}`,
+         asyncAmdStatusCallback: `https://yourdomain.com/path/to/AMDResponseWebhookEndpoint.js?agentCallSid=${agentCall.sid}`, // the webhook endpoint.
          asyncAmd: true,
          to: userNum,
          from: callerId
